01ce83dbc02283d300a7aff6d49a89fd5b672e6c
markopolo <113996049+at7f1i@users.noreply.github.com>
2024-10-01 21:08:34 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "bytegrad.com",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
